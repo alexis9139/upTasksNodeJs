bb0fc929f616daaff12df4167ccdf68c6d5c9c64
Alexis <alexismedrano9139@gmail.com>
2021-01-11 17:31:56 -0300

    Creacion de los controllers, y modificacion de las rutas

---
+++ controllers/proyectosController.js
@@ -0,0 +1,7 @@
+exports.proyectosHome = (request,response)=>{
+    response.send('Index');
+}
+
+exports.proyectosNosotros = (request,response)=>{
+    response.send('Nosotros1');
+}
--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,15 @@ const express = require('express');//importamos express
 
 const router = express.Router();
 
+//importar el controlador
+const proyectosController = require('../controllers/proyectosController');
+
 module.exports = function(){
     //ruta de la home
-    router.get('/', (request,response)=>{
-        response.send('Index');
-    });
+    router.get('/', proyectosController.proyectosHome);
+
     //ruta de nosotros
-    router.get('/nosotros', (request,response)=>{
-        response.send('Nosotros');
-    });
+    router.get('/nosotros', proyectosController.proyectosNosotros);
     //retorno el router para hacelo disponible
     return router;
 };
